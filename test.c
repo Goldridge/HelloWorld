7b2f9691de2b91af162403b01eae9d15f26552c8
emnqqtj <cheng.zhang@ericsson.com>
2016-12-15 15:54:24 +0100

    One commit now.

--- test.c
+++ test.c
@@ -1,13 +1,27 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+int fib(int n)
+{
+	if (n == 1)
+		return 1;
+	return n*fib(n-1);
+}
+
 int main(int argc, char **argv)
 {
+	int k;
+
     if(argc < 2)
     {
        printf("Usage: %s  <argument>\n",  argv[0]);
        exit(-1);
     }
-    printf("%s %s\n", argv[0], argv[1]);
+
+    k=atoi(argv[1]);
+    printf("---------------------------------\n");
+    printf("  Fibonaci(%d)=%d\n", k, fib(k));
+    printf("---------------------------------\n");
+
     return 0;
 }
