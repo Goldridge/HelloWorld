6ed67de8cea22a3b403c0abf936ece9dffc6e798
emnqqtj <cheng.zhang@ericsson.com>
2016-12-21 16:55:04 +0100

    Add a Fibonacci function.

--- test.c
+++ test.c
@@ -1,6 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+int fib(int n)
+{
+	if (n==1)
+		return 1;
+	return n*fib(n-1);
+}
+
 int main(int argc, char **argv)
 {
     if(argc < 2)
@@ -8,6 +15,9 @@ int main(int argc, char **argv)
        printf("Usage: %s  <argument>\n",  argv[0]);
        exit(-1);
     }
-    printf("%s %s\n", argv[0], argv[1]);
+
+    int n = atoi(argv[1]);
+
+    printf("fib(%d) = %d\n", n, fib(n));
     return 0;
 }
